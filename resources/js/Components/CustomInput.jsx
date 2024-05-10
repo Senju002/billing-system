58f5e0ae57ade0548964636e100ac2f59169a317
unknown <rajkaran.mei34@gmail.com>
2024-05-10 08:09:58 +0700

    CustomInput component

---
+++ resources/js/Components/CustomInput.jsx
@@ -0,0 +1,29 @@
+import { Input } from "@material-tailwind/react";
+import React from "react";
+
+export default function CustomInput({
+    label,
+    id,
+    value,
+    onChange,
+    color,
+    errors,
+    className,
+}) {
+    return (
+        <div className="flex flex-col w-full mr-4 ">
+            <div className={`lg:w-full ml-0 ${className}`}>
+                <Input
+                    label={label}
+                    id={id}
+                    value={value}
+                    onChange={onChange}
+                    color={color}
+                />
+            </div>
+            {errors && (
+                <p className="text-red-500 text-sm ml-0 mt-3">{errors}</p>
+            )}
+        </div>
+    );
+}
