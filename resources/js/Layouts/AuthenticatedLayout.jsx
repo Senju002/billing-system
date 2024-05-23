de6226514ed4933f7ff245709b6fe61715637a26
Senju002 <rajkaran.mei34@gmail.com>
2024-05-23 16:01:58 +0700

    making a top navbar to render all of the content in the table

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -38,35 +38,40 @@ export default function Authenticated({ auth, header, children }) {
     const closeDrawer = () => setIsDrawerOpen(false);
 
     return (
-        <div className="min-h-screen bg-gray-100 flex flex-row tablet:flex-col">
-            <Sidebar
-                user={auth.user.name}
-                classname=" tablet:hidden sticky top-0"
-            />
+        <div className="min-h-screen bg-gray-100 flex flex-col tablet:flex-col">
+            <div className="fixed top-0 left-0 w-full z-10 bg-primary shadow-[0_1px_100px_#c3b0f7]">
+                <div className=" flexs flex-row items-center px-4 flex justify-between">
+                    <div className="flex justify-between items-center py-2 ">
+                        <IconButton
+                            variant="text"
+                            size="lg"
+                            onClick={openDrawer}
+                            className="block text-white"
+                        >
+                            {isDrawerOpen ? (
+                                <XMarkIcon className="h-8 w-8 stroke-2" />
+                            ) : (
+                                <Bars3Icon className="h-8 w-8 stroke-2" />
+                            )}
+                        </IconButton>
+                        <Drawer
+                            open={isDrawerOpen}
+                            onClose={closeDrawer}
+                            className="bg-opacity-0"
+                        >
+                            <Sidebar user={auth.user.name} classname="" />
+                        </Drawer>
+                    </div>
 
-            <>
-                <IconButton
-                    variant="text"
-                    size="lg"
-                    onClick={openDrawer}
-                    className="hidden tablet:block bg-red-500"
-                >
-                    {isDrawerOpen ? (
-                        <XMarkIcon className="h-8 w-8 stroke-2" />
-                    ) : (
-                        <Bars3Icon className="h-8 w-8 stroke-2" />
-                    )}
-                </IconButton>
-                <Drawer
-                    open={isDrawerOpen}
-                    onClose={closeDrawer}
-                    className="desktop:hidden"
-                >
-                    <Sidebar user={auth.user.name} classname="desktop:hidden" />
-                </Drawer>
-            </>
-
-            <main className="w-full overflow-auto">{children}</main>
+                    <Typography className="text-textColor  font-extrabold">
+                        BILLING SYSTEM
+                    </Typography>
+                    <div className="text-primary">Billing Sistem</div>
+                </div>
+            </div>
+            <main className={`pt-16 ${isDrawerOpen ? "filter blur-sm" : ""}`}>
+                {children}
+            </main>
         </div>
     );
 }
