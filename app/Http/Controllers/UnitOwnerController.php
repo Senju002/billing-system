0ba8d5e8b6193a7f62dfa6ac6ac4e831351f6355
Senju002 <rajkaran.mei34@gmail.com>
2024-05-31 16:09:37 +0700

    making all of the unit owner functionality is only specific to one admin on each apartment

--- app/Http/Controllers/UnitOwnerController.php
+++ app/Http/Controllers/UnitOwnerController.php
@@ -12,14 +12,22 @@ class UnitOwnerController extends Controller
 {
     public function index(Request $request)
     {
+        $user = Auth::user();
+        $role = $user->role;
+
+        $query = ApartmentOwner::with(['apartment', 'createdBy'])
+            ->when($role !== 'SUPER ADMIN', function ($query) use ($user) {
+                return $query->where('apartment_id', $user->apartment_id);
+            })
+            ->when($request->has('search'), function ($query) use ($request) {
+                return $query->where('owner_name', 'like', "%" . $request->input('search') . "%");
+            })
+            ->orderByDesc('id')
+            ->paginate(10);
+
         return Inertia::render('Unit Owner/UnitOwner', [
             'filters' => $request->only('search'),
-            'data' => ApartmentOwner::with(['apartment', 'createdBy']) // Eager load the apartment and createdBy relationships
-                ->when($request->has('search'), function ($query) use ($request) {
-                    $query->where('owner_name', 'like', "%" . $request->input('search') . "%");
-                })
-                ->orderByDesc('id')
-                ->paginate(10),
+            'data' => $query,
         ]);
     }
 
@@ -65,10 +73,28 @@ class UnitOwnerController extends Controller
             return ['label' => $apartmentName, 'value' => $apartementId];
         })->prepend(['label' => 'Pilih Apartemen', 'value' => ''])->values()->toArray();
 
-        return Inertia::render('Unit Owner/EditUnitOwner', [
-            "unitOwnerData" => $apartmentOwner->find($request->id),
-            'apartmenetData' => $apartment,
-        ]);
+        $user = Auth::user();
+        $unitOwnerData = $apartmentOwner->find($request->id);
+        $unitOwnerApartmentData = $unitOwnerData->apartment_id;
+
+        $userApartmentId = $user->apartment_id;
+        $role = $user->role;
+
+        if ($role === 'SUPER ADMIN') {
+            return Inertia::render('Unit Owner/EditUnitOwner', [
+                "unitOwnerData" => $apartmentOwner->find($request->id),
+                'apartmenetData' => $apartment,
+            ]);
+        } else {
+            if ($userApartmentId == $unitOwnerApartmentData) {
+                return Inertia::render('Unit Owner/EditUnitOwner', [
+                    "unitOwnerData" => $apartmentOwner->find($request->id),
+                    'apartmenetData' => $apartment,
+                ]);
+            } else {
+                return redirect('/unauthorized');
+            }
+        }
     }
 
     public function update(Request $request, $id)
